cfd66e4a69bee3483629acba49be57c951f04eee
Leach <sl02297@heron087.eps.surrey.ac.uk>
2023-09-29 15:01:41 +0100

    fixed style errors 4

--- ProblemSets/src/main/java/ProblemSet_2c/Module.java
+++ ProblemSets/src/main/java/ProblemSet_2c/Module.java
@@ -11,7 +11,7 @@ public class Module {
 	private Assessment assessment1;
 	private Assessment assessment2;
 	private Assessment assessment3;
-	public Module (Assessment ass1, Assessment ass2, Assessment ass3) {
+	public Module(Assessment ass1, Assessment ass2, Assessment ass3) {
 		assessment1 = ass1;
 		assessment2 = ass2;
 		assessment3 = ass3;
@@ -19,7 +19,7 @@ public class Module {
 	public double calculateAverage() {
 		double average = assessment1.getMark();
 		average += assessment2.getMark();
-		average = (average/2) * 0.4;
+		average = (average / 2) * 0.4;
 		average += (assessment3.getMark()) * 0.6;
 		return average;
 	}
---
+++ ProblemSets/src/main/java/ProblemSet_3a/Position.java
@@ -0,0 +1,5 @@
+package ProblemSet_3a;
+
+public class Position {
+
+}
