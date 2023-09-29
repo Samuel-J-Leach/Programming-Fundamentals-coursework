3ea791b6aafcd310fd8816d7f66e539cd17ae02b
Leach <sl02297@heron087.eps.surrey.ac.uk>
2023-09-29 15:09:14 +0100

    completed problem set 3a

--- ProblemSets/src/main/java/ProblemSet_3a/Position.java
+++ ProblemSets/src/main/java/ProblemSet_3a/Position.java
@@ -1,5 +1,18 @@
+//Postition.java
+
 package ProblemSet_3a;
 
-public class Position {
+/*
+ * a simple class that stores the name of a position in a company
+ */
 
+public class Position {
+	public String roleName;
+	public Position() {}
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String newRoleName) {
+		roleName = newRoleName;
+	}
 }
